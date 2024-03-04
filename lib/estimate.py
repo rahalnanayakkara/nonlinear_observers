68a78524537319eabe8cd4f37c02951757c316f5
Rahal <rahaltharak@gmail.com>
2024-03-04 00:15:41 -0800

    Time Varying SIR example

--- lib/estimate.py
+++ lib/estimate.py
@@ -1,6 +1,8 @@
 import numpy as np
 from moving_polyfit.moving_ls import PolyEstimator
+from moving_gauss import GaussEstimator
 from lib.func import generate_lagrange, deriv_bound
+import matplotlib.pyplot as plt
 
 def get_poly_estimates(t_samples, y_samples, Y_max, window_size, sampling_dt, d, delay, nderivs, integration_dt):
 
@@ -18,9 +20,43 @@ def get_poly_estimates(t_samples, y_samples, Y_max, window_size, sampling_dt, d,
         t0 = t_samples[i-window_size+1]
         estimator.fit(y_samples[i-window_size+1:i+1])
         res_pol = np.array(lagrange_pols)@estimator.residuals[l_indices]
-        t = np.linspace(window_size-2-delay, window_size-1-delay, num_steps_per_sample+1)
+        t = np.linspace(window_size-2-delay, window_size-1-delay, num_steps_per_sample+1)*sampling_dt
         for j in range(nderivs):
             y_hat[j, (i-delay-1)*num_steps_per_sample : (i-delay)*num_steps_per_sample+1] = estimator.differentiate(t, j)
             y_bound[j, (i-delay-1)*num_steps_per_sample : (i-delay)*num_steps_per_sample+1]  = np.abs(res_pol.deriv(j)(t))+deriv_bound(k=j, d=d, M=Y_max[d+1], delta_s=sampling_dt)
+
+    return y_hat, y_bound
+
+def get_gauss_estimates(t_samples, y_samples, Y_max, window_size, sampling_dt, n, delay, nderivs, integration_dt):
+
+    num_samples = len(y_samples)
+    num_steps_per_sample = int(sampling_dt/integration_dt)
+    num_steps = (num_samples-1)*num_steps_per_sample
+
+    estimator = GaussEstimator(n=n, N=window_size, dt=sampling_dt)
+    
+    d_list = list(range(nderivs))   # to estimate kth derivative, we need d=k
+    d_list[0] = 1   # for 0th derivative we still need d=1
+
+    lagrange_pols = []  # store lagrange polynomials for each case of d
+    l_indices = []      # store indices of set D for each case of d
+    for d in d_list:
+        lp, li = generate_lagrange(d=d, N=window_size, sampling_dt=sampling_dt)
+        lagrange_pols.append(lp)
+        l_indices.append(li)
+    
+    y_hat = np.zeros((nderivs, num_steps+1))
+    y_bound = np.zeros((nderivs, num_steps+1))
+
+    # Sliding window of above size
+    for i in range(window_size-1, num_samples):
+        t0 = t_samples[i-window_size+1]
+        estimator.fit(y_samples[i-window_size+1:i+1])
+
+        t = np.linspace(window_size-2-delay, window_size-1-delay, num_steps_per_sample+1)*sampling_dt
+        for j in range(nderivs):
+            res_pol = np.array(lagrange_pols[j])@estimator.residuals[l_indices[j]]
+            y_hat[j, (i-delay-1)*num_steps_per_sample : (i-delay)*num_steps_per_sample+1] = estimator.differentiate(t, j)
+            # y_bound[j, (i-delay-1)*num_steps_per_sample : (i-delay)*num_steps_per_sample+1]  = np.abs(res_pol.deriv(j)(t))+deriv_bound(k=j, d=max(j,1), M=Y_max[d+1]+G_max, delta_s=sampling_dt)
 
     return y_hat, y_bound
--- nonlinear_system/epidem_odes.py
+++ nonlinear_system/epidem_odes.py
@@ -178,3 +178,61 @@ class SIR(ControlAffineODE):
             sol
         ])
         return xhat
+    
+class SIR_t(ControlAffineODE):
+
+    def __init__(self,  mu, beta, gamma, nu, w):
+        self.beta = beta
+        self.mu = mu
+        self.gamma = gamma
+        self.nu = nu
+        self.w = w
+        self.nderivs = 2
+        super().__init__(state_dim=2, input_dim=1, output_dim=2, f=self.sir_f, h=self.output_derivative)
+    
+    def sir_f(self, t: float, x: np.ndarray):
+        '''
+        RHS for SIR system:
+
+        d_x0 = mu - mu*x0 - beta*x0*x1
+        d_x1 = beta*x0*x1 - (mu+gamma)*x1
+
+        '''
+        beta = self.beta*(1+self.nu*np.cos(self.w*t))
+        rhs = np.array([
+            self.mu - self.mu*x[0] - beta*x[0]*x[1],
+            beta*x[0]*x[1] - (self.mu + self.gamma)*x[1]
+        ])
+        return rhs
+    
+    def output_fn(self, t: float, x: np.ndarray, u: np.ndarray):
+        return self.self.beta*(1+self.nu*np.cos(self.w*t))*x[0]*x[1]
+    
+    def output_derivative(self, t: float, x: np.ndarray, u: np.ndarray):
+        '''
+        Computes the output of the system and it's derivatives
+        '''
+        beta = self.beta*(1+self.nu*np.cos(self.w*t))
+        y_d = np.empty((self.nderivs,))
+        y_d[0] = beta*x[0]*x[1]
+        y_d[1] = beta*(x[0]*self.rhs(t, x, u)[1] + x[1]*self.rhs(t, x, u)[0]) + self.beta*(-self.nu*self.w*np.sin(self.w*t))*x[0]*x[1]
+        return y_d
+    
+    def invert_output(self, t: float, y_d: np.ndarray, u: np.ndarray = None):
+        '''
+        Function that maps the output and it's derivatives to the system states
+        '''
+        beta = self.beta*(1+self.nu*np.cos(self.w*t))
+        beta_dot = -self.beta*self.nu*self.w*np.sin(self.w*t)
+
+        a = beta*(self.mu-y_d[0])
+        b = -(2*self.mu+self.gamma)*y_d[0]-y_d[1] + y_d[0]*beta_dot/beta 
+        c = y_d[0]**2
+
+        sol1, sol2 = quad_roots(a,b,c)
+        sol = sol1 if np.max(sol1)<=1 and np.min(sol1)>=0 else sol2
+        xhat = np.array([
+            y_d[0]/(beta*sol),
+            sol
+        ])
+        return xhat
