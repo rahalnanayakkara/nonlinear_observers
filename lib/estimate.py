87600ddcf18ef16c3231b6e971897eb6db5ec78c
Rahal <rahaltharak@gmail.com>
2024-03-06 19:09:03 -0800

    higher derivatives of gaussian

--- lib/estimate.py
+++ lib/estimate.py
@@ -28,14 +28,14 @@ def get_poly_estimates(t_samples, y_samples, Y_max, window_size, sampling_dt, d,
 
     return y_hat, y_bound
 
-def get_gauss_estimates(t_samples, y_samples, Y_max, window_size, sampling_dt, n, delay, nderivs, integration_dt, d=-1):
+def get_gauss_estimates(t_samples, y_samples, Y_max, window_size, sampling_dt, n, delay, nderivs, integration_dt, d0=-1):
 
     num_samples = len(y_samples)
     num_steps_per_sample = int(sampling_dt/integration_dt)
     num_steps = (num_samples-1)*num_steps_per_sample
 
     estimator = GaussEstimator(n=n, N=window_size, dt=sampling_dt)
-    G_max = np.zeros(max(d+2,nderivs+1))
+    G_max = np.zeros(max(d0+2,nderivs+1))
     
     d_list = list(range(nderivs))   # to estimate kth derivative, we need d=k
     d_list[0] = 1   # for 0th derivative we still need d=1
@@ -56,13 +56,13 @@ def get_gauss_estimates(t_samples, y_samples, Y_max, window_size, sampling_dt, n
         estimator.fit(y_samples[i-window_size+1:i+1])
 
         t = np.linspace(window_size-2-delay, window_size-1-delay, num_steps_per_sample+1)*sampling_dt
-        for j in range(max(d+2,nderivs+1)):
+        for j in range(max(d0+2,nderivs+1)):
             G_max[j] = np.max(np.abs(estimator.differentiate(t, j)))
 
         for j in range(nderivs):
             res_pol = np.array(lagrange_pols[j])@estimator.residuals[l_indices[j]]
             y_hat[j, (i-delay-1)*num_steps_per_sample : (i-delay)*num_steps_per_sample+1] = estimator.differentiate(t, j)
-            d = max(j,1) if d<1 else d
+            d = max(j,1) if d0<1 else d0
             y_bound[j, (i-delay-1)*num_steps_per_sample : (i-delay)*num_steps_per_sample+1]  = np.abs(res_pol.deriv(j)(t))+deriv_bound(k=j, d=d, M=Y_max[d+1]+G_max[d+1], delta_s=sampling_dt)
             # print(j, d, Y_max[d+1]+G_max[d+1], deriv_bound(k=j, d=d, M=Y_max[d+1]+G_max[d+1], delta_s=sampling_dt))
 
--- moving_gauss.py
+++ moving_gauss.py
@@ -1,15 +1,21 @@
 import numpy as np
 
-def gauss_func(x, sigma=1, deriv=0):
+def gauss_func(x, s=1, deriv=0):
     if deriv==0:
-        return np.exp(-(x)**2/(2*sigma))
+        return np.exp(-(x)**2/(2*s))
     if deriv==1:
-        return -x*np.exp(-(x)**2/(2*sigma))/sigma
+        return -x*np.exp(-(x)**2/(2*s))/s
     if deriv==2:
-        return np.exp(-(x)**2/(2*sigma))*(x*x-sigma)/(sigma*sigma)
+        return np.exp(-(x)**2/(2*s))*(x*x-s)/(s*s)
     if deriv==3:
-        return -np.exp(-(x)**2/(2*sigma))*(x*x*x-3*sigma*x)/(sigma*sigma*sigma)
-    return np.exp(-(x)**2/(2*sigma))
+        return -np.exp(-(x)**2/(2*s))*(x*x*x-3*s*x)/(s*s*s)
+    if deriv==4:
+        return (np.power(x,4) - 6*s*np.power(x,2)+3*s*s)*gauss_func(x,s,0)/np.power(s,4)
+    if deriv==5:
+        return -(np.power(x,5) - 10*s*np.power(x,3) + 15*s*s*x)*gauss_func(x,s,0)/np.power(s,5)
+    if deriv==6:
+        return (np.power(x,6) - 15*s*np.power(x,4) + 45*s*s*x*x-15*np.power(s,3))*gauss_func(x,s,0)/np.power(s,6)
+    return 0
 
 
 class GaussEstimator:
@@ -37,13 +43,13 @@ class GaussEstimator:
         self.t0 = t0
         self.ti = np.linspace(self.t0, self.t0+self.n*self.dt, self.n, endpoint=False).reshape(-1,1)
         t = np.linspace(t0, t0+self.N*self.dt, self.N, endpoint=False)
-        A = gauss_func((t-self.ti).T, sigma=self.sigma)
+        A = gauss_func((t-self.ti).T, s=self.sigma)
         self.theta, _, _, _ = np.linalg.lstsq(A, y, rcond=None)
         self.residuals = y - A @ self.theta
         return self.theta
     
     def estimate(self, t):
-        return self.theta @ gauss_func(t-self.ti, sigma=self.sigma)
+        return self.theta @ gauss_func(t-self.ti, s=self.sigma)
     
     def differentiate(self, t, q):
-        return self.theta @ gauss_func(t-self.ti, sigma=self.sigma, deriv=q)
+        return self.theta @ gauss_func(t-self.ti, s=self.sigma, deriv=q)
