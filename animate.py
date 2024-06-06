b7bc1d7e1a293e40fe68968c49359a622b9d81ef
rahalnanayakkara <rahaltharak@gmail.com>
2024-06-05 17:37:42 -0700

    Animation with estimate

--- animate.py
+++ animate.py
@@ -3,20 +3,37 @@ import sys
 import numpy as np
 import math
 
+from nonlinear_system.sample_odes import AckermanModel
+from moving_polyfit.moving_ls import MultiDimPolyEstimator
+
 # Initialize Pygame
 pygame.init()
 
 # Screen dimensions
-WIDTH, HEIGHT = 800.0, 600.0
+WIDTH, HEIGHT = 1600.0, 1200.0
 FPS = 30
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Animation")
 
+# Estimator Parameters
+window_length = 10  # Number of samples
+delay = 1
+sampling_dt = 1.0/FPS
+eval_time = (window_length-1-delay)*sampling_dt
+window_times = np.linspace(0., window_length*sampling_dt, window_length, endpoint=False)
+
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREY = (100, 100, 100)
 BLUE = (100, 100, 255)
 YELLOW = (255, 255, 150)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+# Font Settings
+font_size = 25
+font = pygame.font.Font(None, font_size)
 
 # Car Properties
 axle_sep = 60.0
@@ -45,6 +62,13 @@ car_rf = car_rr + np.array([car_len, 0.15*car_width])
 car_lf = car_lr + np.array([car_len, -0.15*car_width])
 car_pts = np.vstack([car_rr, car_lr, car_lf, car_rf])
 
+ODE = AckermanModel(axle_sep, wheel_sep)
+ode_n = ODE.n  # system state dimension
+ode_m = ODE.m  # control input dimension
+ode_p = ODE.p  # output dimension
+est_d = 2
+poly_estimator = MultiDimPolyEstimator(ode_p, est_d, window_length, sampling_dt)
+
 
 def blit_rotate_center(surf, image, topleft, angle):
     rotated_image = pygame.transform.rotate(image, angle)
@@ -56,12 +80,17 @@ def blit_rotate_center(surf, image, topleft, angle):
 class CarSprite(pygame.sprite.Sprite):
     def __init__(self, center):
         super().__init__()
-        self.state = np.zeros(5)
+        self.state = np.zeros(ode_n)
         self.center = center
         self.image = self.create_image()
         self.rect = self.image.get_rect(center=self.center)
         self.state[0] = self.rect.x + self.image.get_rect().width/2
         self.state[1] = self.rect.y + self.image.get_rect().height/2
+
+        self.op_data = np.vstack((np.ones(window_length, dtype=np.float32)*self.state[0], np.ones(window_length, dtype=np.float32)*self.state[1]))
+        self.yhat = np.zeros((2, est_d+1))
+        self.xhat = np.zeros(ode_n)
+        self.u = np.zeros((2, 1))
 
     def create_image(self):
         max_width = 2*car_width
@@ -82,18 +111,23 @@ class CarSprite(pygame.sprite.Sprite):
         return image
 
     def update(self, keys):
+        self.u[:, :] = 0
         if keys[pygame.K_UP]:
             if self.state[3] < speed_limit:
-                self.state[3] += 0.1
+                self.u[0, 0] = 0.1
         if keys[pygame.K_DOWN]:
-            if self.state[3] > -speed_limit:
-                self.state[3] -= 0.1
+            if self.state[3] > 0:
+                self.u[0, 0] = -0.1
         if keys[pygame.K_LEFT]:
             if self.state[4] < math.radians(steering_limit):
-                self.state[4] += math.radians(2)
+                self.u[1, 0] = math.radians(2)
         if keys[pygame.K_RIGHT]:
             if self.state[4] > -math.radians(steering_limit):
-                self.state[4] -= math.radians(2)
+                self.u[1, 0] = -math.radians(2)
+
+        self.state[3] += self.u[0, 0]
+        self.state[4] += self.u[1, 0]
+        self.op_data = np.roll(self.op_data, -1)
 
         self.image = pygame.transform.rotate(self.create_image(), math.degrees(self.state[2]))
         self.rect = self.image.get_rect(center=self.rect.center)
@@ -105,7 +139,60 @@ class CarSprite(pygame.sprite.Sprite):
         self.rect.x = self.state[0] - self.image.get_rect().width/2
         self.rect.y = self.state[1] - self.image.get_rect().height/2
 
-        print(self.rect.x, self.rect.y)
+        # Poly Estimator
+        self.op_data[0, -1] = self.state[0]
+        self.op_data[1, -1] = self.state[1]
+        poly_estimator.fit(self.op_data)
+
+        for i in range(est_d+1):
+            self.yhat[:, i] = poly_estimator.differentiate(eval_time, i)
+
+        self.xhat = ODE.invert_position(0, self.yhat, self.u)
+        self.xhat[3] /= FPS
+        if self.state[3] < 0:
+            self.xhat[3] *= -1
+        self.xhat[2] = -math.pi/2 - self.xhat[2]
+        self.xhat[4] *= -1
+
+
+def draw_text(text_string, top, left):
+    txt_surf = font.render(text_string, True, BLACK)
+    txt_rect = txt_surf.get_rect()
+    txt_rect.top = top
+    txt_rect.left = left
+    screen.blit(txt_surf, txt_rect)
+
+
+def draw_speed_bar(speed, speed_est):
+    BAR_TOP = 10
+    BAR_LEFT = 20
+    BAR_LEN = 200
+    BAR_WIDTH = 20
+    TXT_OFFSET = 5
+    speed_pos = speed/speed_limit*BAR_LEN+BAR_LEFT
+    speed_pos_est = speed_est/speed_limit*BAR_LEN+BAR_LEFT
+    pygame.draw.rect(screen, BLACK, (BAR_LEFT, BAR_TOP, BAR_LEN, BAR_WIDTH), width=1)
+    # pygame.draw.line(screen, GREY, (BAR_LEFT+BAR_LEN/2, BAR_TOP), (BAR_LEFT+BAR_LEN/2, BAR_TOP+BAR_WIDTH))
+    pygame.draw.line(screen, RED, (speed_pos, BAR_TOP), (speed_pos, BAR_TOP+BAR_WIDTH), width=2)
+    pygame.draw.line(screen, GREEN, (speed_pos_est, BAR_TOP), (speed_pos_est, BAR_TOP+BAR_WIDTH), width=2)
+    draw_text(str(0), BAR_TOP+BAR_WIDTH+TXT_OFFSET, BAR_LEFT-TXT_OFFSET)
+    draw_text(str(speed_limit), BAR_TOP+BAR_WIDTH+TXT_OFFSET, BAR_LEN+BAR_LEFT-TXT_OFFSET)
+    # draw_text(str(0), BAR_TOP+BAR_WIDTH+TXT_OFFSET, BAR_LEFT+BAR_LEN/2-TXT_OFFSET)
+
+
+def draw_steering_circle(angle, angle_est):
+    CENTER_X = 120
+    CENTER_Y = 160
+    RAD = 80
+    # pygame.draw.circle(screen, BLACK, (CENTER_X, CENTER_Y), RAD, width=1)
+    pygame.draw.arc(screen, BLACK, ((CENTER_X-RAD, CENTER_Y-RAD), (2*RAD, 2*RAD)), 0, math.pi)
+    pygame.draw.line(screen, BLACK, (CENTER_X-RAD, CENTER_Y), (CENTER_X+RAD, CENTER_Y))
+    pygame.draw.line(screen, GREY, (CENTER_X, CENTER_Y), (CENTER_X, CENTER_Y-RAD))
+    pygame.draw.line(screen, RED, (CENTER_X, CENTER_Y), (CENTER_X-RAD*math.sin(angle), CENTER_Y-RAD*math.cos(angle)), width=2)
+    pygame.draw.line(screen, GREEN, (CENTER_X, CENTER_Y), (CENTER_X-RAD*math.sin(angle_est), CENTER_Y-RAD*math.cos(angle_est)), width=2)
+    draw_text(str(0), CENTER_Y-RAD-15, CENTER_X-5)
+    draw_text(str(90), CENTER_Y-10, CENTER_X+RAD+5)
+    draw_text(str(-90), CENTER_Y-10, CENTER_X-RAD-25)
 
 
 # Clock to control the frame rate
@@ -130,6 +217,14 @@ while running:
 
     screen.fill(WHITE)
     all_sprites.draw(screen)
+
+    pygame.draw.line(screen, RED, (sprite.state[0], sprite.state[1]), (sprite.state[0]-80*math.sin(sprite.state[2]), sprite.state[1]-80*math.cos(sprite.state[2])), width=2)
+    pygame.draw.line(screen, GREEN, (sprite.state[0], sprite.state[1]), (sprite.state[0]-80*math.sin(sprite.xhat[2]), sprite.state[1]-80*math.cos(sprite.xhat[2])), width=2)
+    # pygame.draw.circle(screen, RED, (sprite.xhat[0], sprite.xhat[1]), 10)
+    # draw_speed_bar(sprite.state[3])
+
+    draw_speed_bar(sprite.state[3], sprite.xhat[3])
+    draw_steering_circle(sprite.state[4], sprite.xhat[4])
 
     pygame.display.flip()
     clock.tick(FPS)
